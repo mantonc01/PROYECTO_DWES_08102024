034537b63538391ad0dca182ea74b9cd45c3a445
mantonc01 <mantonc01@informatica.iesvalledeljerteplasencia.es>
2024-10-08 19:02:49 +0200

    creacion de los partials

--- blog.php
+++ blog.php
@@ -1,3 +1,3 @@
 <?php
-require 'views/blog.view.php'
+require 'views/blog.view.php';
 ?>
--- single_post.php
+++ single_post.php
@@ -1,5 +1,5 @@
 <?php
-require 'views/single_post.view.php'
+require 'views/single_post.view.php';
 //realizando cambios para probar que solo hay una rama principal
 // CAMBIO EL NOMBRE DEL GIT
 ?>
