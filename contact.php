11a61ae785242acedc69f45e8fd90e03e9d73938
mantonc01 <mantonc01@informatica.iesvalledeljerteplasencia.es>
2024-10-09 12:16:14 +0200

    realizando ejercicio 1

--- contact.php
+++ contact.php
@@ -1,16 +1,27 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "post") {
-    $validacion = true;
+$errores = [];
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    //$validacion = true;
     $nombre = htmlspecialchars(trim($_POST['nombre']));
     $apellido = htmlspecialchars(trim($_POST['apellido']));
     $email = htmlspecialchars(trim($_POST['email']));
     $sujeto = htmlspecialchars(trim($_POST['sujeto']));
     $textArea = htmlspecialchars(trim($_POST['textArea']));
-    if (!empty($nombre) && !empty($email) && !empty($sujeto)) {
+    if (empty($nombre) ) {
         
-    } else {
-        echo "<div> <p>campos vacios</p></div>";
-        $validacion = false;
+        $errores[]='El campo First Name no puede estar vacío.';
+    } ;
+    if (empty($email) ) {
+        
+        $errores[]='El campo Email no puede estar vacío.';
+    } ;
+    if (empty($sujeto) ) {
+        
+        $errores[]='El campo subjet no puede estar vacío.';
+    } ;
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errores[]='Email incorrecto';
     }
+    
 }
 require 'views/contact.view.php';
--- views/contact.view.php
+++ views/contact.view.php
@@ -1,92 +1,105 @@
-<?php include __DIR__.'/partials/inicio-doc.part.php'?>
+<?php include __DIR__ . '/partials/inicio-doc.part.php' ?>
 
 <!-- Navigation Bar -->
-   <nav class="navbar navbar-fixed-top navbar-default">
-     <div class="container">
-	   	 <div class="navbar-header">
-	   	 	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
-	   	 		<span class="sr-only">Toggle navigation</span>
-	   	 		<span class="icon-bar"></span>
-	   	 		<span class="icon-bar"></span>
-	   	 		<span class="icon-bar"></span>
-	   	 	</button>
-            <a  class="navbar-brand page-scroll" href="#page-top">
-              <span>[PHOTO]</span>
-            </a>
-	   	 </div>
-	   	 <div class="collapse navbar-collapse navbar-right" id="menu">
-	   	 	<ul class="nav navbar-nav">
-	   	 		<li class=" lien"><a href="index.php"><i class="fa fa-home sr-icons"></i> Home</a></li>
-	   	 		<li class="lien"><a href="about.php"><i class="fa fa-bookmark sr-icons"></i> About</a></li>
-	   	 		<li class="lien"><a href="blog.php"><i class="fa fa-file-text sr-icons"></i> Blog</a></li>
-	   	 		<li class="active"><a href="#"><i class="fa fa-phone-square sr-icons"></i> Contact</a></li>
-	   	 	</ul>
-	   	 </div>
-   	 </div>
-   </nav>
+<nav class="navbar navbar-fixed-top navbar-default">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand page-scroll" href="#page-top">
+				<span>[PHOTO]</span>
+			</a>
+		</div>
+		<div class="collapse navbar-collapse navbar-right" id="menu">
+			<ul class="nav navbar-nav">
+				<li class=" lien"><a href="index.php"><i class="fa fa-home sr-icons"></i> Home</a></li>
+				<li class="lien"><a href="about.php"><i class="fa fa-bookmark sr-icons"></i> About</a></li>
+				<li class="lien"><a href="blog.php"><i class="fa fa-file-text sr-icons"></i> Blog</a></li>
+				<li class="active"><a href="#"><i class="fa fa-phone-square sr-icons"></i> Contact</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
 <!-- End of Navigation Bar -->
+<?php //echo '<script>alert("Welcome to Geeks for Geeks")</script>';//include __DIR__.'/../contact.php'
+?>
 
 <!-- Principal Content Start -->
-   <div id="contact">
-   	  <div class="container">
-   	    <div class="col-xs-12 col-sm-8 col-sm-push-2">
-       	   <h1>CONTACT US</h1>
-       	   <hr>
-       	   <p>Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
-	       <form method="post" class="form-horizontal">
-	       	  <div class="form-group">
-	       	  	<div class="col-xs-6">
-	       	  	    <label class="label-control">First Name</label>
-	       	  		<input class="form-control" type="text" name="nombre" value="<?php if(isset($nombre)) echo $nombre?>">
-	       	  	</div>
-	       	  	<div class="col-xs-6">
-	       	  	    <label class="label-control">Last Name</label>
-	       	  		<input class="form-control" type="text" name="apellido" value="<?php if(isset($apellido)) echo $apellido?>">
-	       	  	</div>
-	       	  </div>
-	       	  <div class="form-group">
-	       	  	<div class="col-xs-12">
-	       	  		<label class="label-control">Email</label>
-	       	  		<input class="form-control" type="text" name="email" value="<?php if(isset($email)) echo $email?>">
-	       	  	</div>
-	       	  </div>
-	       	  <div class="form-group">
-	       	  	<div class="col-xs-12">
-	       	  		<label class="label-control">Subject</label>
-	       	  		<input class="form-control" type="text" name="sujeto" value="<?php if(isset($sujeto)) echo $sujeto?>">
-	       	  	</div>
-	       	  </div>
-	       	  <div class="form-group">
-	       	  	<div class="col-xs-12">
-	       	  		<label class="label-control">Message</label>
-	       	  		<textarea class="form-control" name="textArea" value="<?php if(isset($textArea)) echo $textArea?>"></textarea>
-	       	  		<button class="pull-right btn btn-lg sr-button">SEND</button>
-	       	  	</div>
-	       	  </div>
-	       </form>
-	       <hr class="divider">
-	       <div class="address">
-	           <h3>GET IN TOUCH</h3>
-	           <hr>
-	           <p>Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero.</p>
-		       <div class="ending text-center">
-			        <ul class="list-inline social-buttons">
-			            <li><a href="#"><i class="fa fa-facebook sr-icons"></i></a>
-			            </li>
-			            <li><a href="#"><i class="fa fa-twitter sr-icons"></i></a>
-			            </li>
-			            <li><a href="#"><i class="fa fa-google-plus sr-icons"></i></a>
-			            </li>
-			        </ul>
-				    <ul class="list-inline contact">
-				       <li class="footer-number"><i class="fa fa-phone sr-icons"></i>  (00228)92229954 </li>
-				       <li><i class="fa fa-envelope sr-icons"></i>  kouvenceslas93@gmail.com</li>
-				    </ul>
-				    <p>Photography Fanatic Template &copy; 2017</p>
-		       </div>
-	       </div>
-	    </div>   
-   	  </div>
-   </div>
+<div id="contact">
+
+
+	<div class="container">
+		<div class="col-xs-12 col-sm-8 col-sm-push-2">
+			<h1>CONTACT US</h1>
+			<hr>
+			<p>Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+			<form method="post" action="contact.php" class="form-horizontal">
+				<?php if (!empty($errores)) : ?>
+					<div class="alert alert-danger">
+						<ul>
+							<?php foreach ($errores as $error) : ?>
+								<li><?php echo $error; ?></li>
+							<?php endforeach; ?>
+						</ul>
+					</div>
+				<?php endif; ?>
+				<div class="form-group">
+					<div class="col-xs-6">
+						<label class="label-control">First Name</label>
+						<input class="form-control" type="text" name="nombre" value="<?php if (isset($nombre)) echo $nombre ?>">
+					</div>
+					<div class="col-xs-6">
+						<label class="label-control">Last Name</label>
+						<input class="form-control" type="text" name="apellido" value="<?php if (isset($apellido)) echo $apellido ?>">
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-xs-12">
+						<label class="label-control">Email</label>
+						<input class="form-control" type="text" name="email" value="<?php if (isset($email)) echo $email ?>">
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-xs-12">
+						<label class="label-control">Subject</label>
+						<input class="form-control" type="text" name="sujeto" value="<?php if (isset($sujeto)) echo $sujeto ?>">
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-xs-12">
+						<label class="label-control">Message</label>
+						<textarea class="form-control" name="textArea" value="<?php if (isset($textArea)) echo $textArea ?>"></textarea>
+						<button class="pull-right btn btn-lg sr-button">SEND</button>
+					</div>
+				</div>
+			</form>
+			<hr class="divider">
+			<div class="address">
+				<h3>GET IN TOUCH</h3>
+				<hr>
+				<p>Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero.</p>
+				<div class="ending text-center">
+					<ul class="list-inline social-buttons">
+						<li><a href="#"><i class="fa fa-facebook sr-icons"></i></a>
+						</li>
+						<li><a href="#"><i class="fa fa-twitter sr-icons"></i></a>
+						</li>
+						<li><a href="#"><i class="fa fa-google-plus sr-icons"></i></a>
+						</li>
+					</ul>
+					<ul class="list-inline contact">
+						<li class="footer-number"><i class="fa fa-phone sr-icons"></i> (00228)92229954 </li>
+						<li><i class="fa fa-envelope sr-icons"></i> kouvenceslas93@gmail.com</li>
+					</ul>
+					<p>Photography Fanatic Template &copy; 2017</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <!-- Principal Content Start -->
-<?php include __DIR__.'/partials/fin-doc.part.php'?>
+<?php include __DIR__ . '/partials/fin-doc.part.php' ?>
